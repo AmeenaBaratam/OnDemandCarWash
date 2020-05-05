c1ed6f7b064acdd676c3f979febf0c3f744dc20d
Ameena, Baratam <ameena.baratam@gmail.com>
2020-05-05 22:57:48 +0530

    first commit

---
+++ odcw/src/main/java/com/casestudy/odcw/OdcwApplication.java
@@ -0,0 +1,13 @@
+package com.casestudy.odcw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdcwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdcwApplication.class, args);
+	}
+
+}
